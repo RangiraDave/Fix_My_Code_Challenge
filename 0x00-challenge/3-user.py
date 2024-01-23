0bd6317e8258459a2cbef13e601e1a31a0bf51d7
RangiraDave <rangiradev666@gmail.com>
2024-01-23 09:24:24 +0200

    Test challenge 3

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -54,7 +54,7 @@ class User():
             return False
         if self.__password is None:
             return False
-        if hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password:
+        if hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password:
             return True
 
 

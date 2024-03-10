470d4e80dbaaa67ffd17db373c20aed869b19d25
RangiraDave <rangiradev666@gmail.com>
2024-03-10 09:36:15 +0200

    corrected challenge nbr 2

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,30 +1,38 @@
 #!/usr/bin/python3
-""" 
-User class
+"""
+User class implementation.
 """
 
-class User():
-    """ Documentation """
 
-    def __init__(self):
-        """ Documentation """
-        self.__email = None
+class User():
+    """ User class definition. """
+
+    __email = ''
+
+    def __init__(self, email=None):
+        """ User class constructor. """
+
+        if email is not None:
+            self.__email = email
+
+    @property
+    def email(self):
+        """ Function to return user email. """
+
+        return self.__email
 
     @email.setter
     def email(self, value):
-        """ Documentation """
+        """ User email setter function. """
+
         if type(value) is not str:
             raise TypeError("email must be a string")
         self.__email = value
 
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
-   
-    
+
 if __name__ == "__main__":
+    """ Cleating User object in the main function. """
 
     u = User()
-    u.email = "john@snow.com"
+    u.email = 'john@snow.com'
     print(u.email)
